daec941e75475dc2faa80a46f0da68c73036bdf9
Tema <108786888+Bednyakov@users.noreply.github.com>
2024-07-21 18:02:54 +0300

    Create background.js

---
+++ proxi_extension/background.js
@@ -0,0 +1,28 @@
+var config = {
+    mode: "fixed_servers",
+    rules: {
+        singleProxy: {
+            scheme: "http",
+            host: "186.65.122.5",
+            port: parseInt("8000")
+        },
+        bypassList: ["localhost"]
+    }
+};
+
+chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+function callbackFn(details) {
+    return {
+        authCredentials: {
+            username: "username",
+            password: "pass"
+        }
+    };
+}
+
+chrome.webRequest.onAuthRequired.addListener(
+    callbackFn,
+    {urls: ["<all_urls>"]},
+    ['blocking']
+);
