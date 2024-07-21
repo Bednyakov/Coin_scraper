2915c88d5732c7c8ec3f4343438aa2d9e40ed4e9
Artem Bednyakov <artb@list.ru>
2024-07-21 15:24:26 +0300

    add requirements.txt

---
+++ config.py
@@ -0,0 +1,13 @@
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Accept-Language': 'ru,en-US;q=0.9,en;q=0.8',
+    'Cookie': '_ga=GA1.1.352939514.1715682647; _gcl_au=1.1.1652464611.1715682647; _cq_duid=1.1715682647.LnzYoJSNhSvLoROy; mp_4e762b32525bd7ecddda43ce51776343_mixpanel=%7B%22distinct_id%22%3A%20%22%24device%3A190a847d25bb86-06dc6fb599f03b-26001f51-15f900-190a847d25bb86%22%2C%22%24device_id%22%3A%20%22190a847d25bb86-06dc6fb599f03b-26001f51-15f900-190a847d25bb86%22%2C%22%24initial_referrer%22%3A%20%22https%3A%2F%2Fwww.coingecko.com%2F%22%2C%22%24initial_referring_domain%22%3A%20%22www.coingecko.com%22%2C%22__mps%22%3A%20%7B%7D%2C%22__mpso%22%3A%20%7B%22%24initial_referrer%22%3A%20%22https%3A%2F%2Fwww.coingecko.com%2F%22%2C%22%24initial_referring_domain%22%3A%20%22www.coingecko.com%22%7D%2C%22__mpus%22%3A%20%7B%7D%2C%22__mpa%22%3A%20%7B%7D%2C%22__mpu%22%3A%20%7B%7D%2C%22__mpr%22%3A%20%5B%5D%2C%22__mpap%22%3A%20%5B%5D%7D; __stripe_mid=5d76b3a2-a73a-41bc-9b7f-e3d8817603e1d848cc; __hstc=234611265.c0618f71f34797815aa0c635269083f5.1720810250103.1720810250103.1720810250103.1; hubspotutk=c0618f71f34797815aa0c635269083f5; __gads=ID=48483377b9e7c490:T=1720812789:RT=1720813863:S=ALNI_MbYJe0cUehODZWKEP-PjDmNaxnvcg; __gpi=UID=00000e73dfed06aa:T=1720812789:RT=1720813863:S=ALNI_MapI29Cn2LeLveXM4VLkdmCmfrCpg; __eoi=ID=893743c20fc09fcf:T=1720812789:RT=1720813863:S=AA-AfjY0qpxloH409lXYS3yE8AEH; _session_id=1678326b18d22cca68a51d59f8399921; OptanonConsent=isGpcEnabled=0&datestamp=Fri+Jul+12+2024+23%3A10%3A17+GMT%2B0300+(%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0%2C+%D1%81%D1%82%D0%B0%D0%BD%D0%B4%D0%B0%D1%80%D1%82%D0%BD%D0%BE%D0%B5+%D0%B2%D1%80%D0%B5%D0%BC%D1%8F)&version=202312.1.0&browserGpcFlag=0&isIABGlobal=false&hosts=&consentId=8c18c027-562f-453e-8818-bf9f19dcaea5&interactionCount=1&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A1%2CC0004%3A1%2CC0003%3A1&AwaitingReconsent=false; _ga_LJR3232ZPB=GS1.1.1720810087.3.1.1720816403.0.0.0; _ga_1Y6C78JXR3=GS1.1.1720810087.3.1.1720816403.0.0.0',
+    'Referer': 'https://www.coingecko.com/',
+}
+
+proxies = {
+    'http': 'http://login:password@ip:port',
+    'https': 'http://login:password@ip:port'
+}
---
+++ phantom.py
@@ -0,0 +1,65 @@
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
+
+
+def get_html(url: str) -> str:
+    """
+    Возвращает HTML.
+    """
+    chrome_options = Options()
+    #chrome_options.add_argument('--headless')  # Запускаем в режиме без графического интерфейса
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.set_page_load_timeout(5)
+
+    try:
+        driver.get(url)
+    except Exception as e:
+        print(e)
+
+    finally:
+
+        # Получаем HTML-код страницы
+        html = driver.page_source
+        driver.quit()
+
+        return html
+
+
+def get_elements(html: str) -> None:
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Находим элемент <tbody>
+    tbody = soup.find('tbody')
+
+    # Извлекаем все элементы <tr> внутри <tbody>
+    rows = tbody.find_all('tr')
+
+    # Проходимся по каждой строке и извлекаем текст и ссылки
+    for row in rows:
+        # Извлекаем текст из всех ячеек <td>
+        cells = row.find_all('td')
+        cell_texts = [cell.get_text(strip=True) for cell in cells if cell.get_text(strip=True) != '']
+
+        # Извлекаем ссылки из всех ячеек <td>
+        cell_links = [cell.find('a')['href'] if cell.find('a') else None for cell in cells]
+        cell_links = {link for link in cell_links if link is not None}
+
+        # Выводим результаты
+        print(f"Текст: {cell_texts}")
+        print(f"Ссылки: {cell_links}")
+        print("-" * 30)
+
+
+def save_html(html: str) -> None:
+    with open('page.html', 'w', encoding='utf-8') as file:
+        file.write(html)
+
+
+if __name__ == '__main__':
+    url_to_scrape = 'https://www.coingecko.com/'
+    html = get_html(url_to_scrape)
+    save_html(html)
+    get_elements(html)
