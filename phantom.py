b2147ec9df484259e6faab504e53409e6a538551
Tema <108786888+Bednyakov@users.noreply.github.com>
2024-07-21 18:04:06 +0300

    Update phantom.py

--- phantom.py
+++ phantom.py
@@ -9,6 +9,7 @@ def get_html(url: str) -> str:
     Возвращает HTML.
     """
     chrome_options = Options()
+    chrome_options.add_argument(f"--load-extension={'/home/artem/Coin_scraper/proxi_extension'}") #  подключаем расширение с прокси
     #chrome_options.add_argument('--headless')  # Запускаем в режиме без графического интерфейса
     driver = webdriver.Chrome(options=chrome_options)
     driver.set_page_load_timeout(5)
